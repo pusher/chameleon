9fbe03b66c52f24634bfc3e7c4103b52fdb7e36c
Alex Pate <alex.pate@pusher.com>
2016-02-26 18:40:27 +0000

    Drop grunt in favour of Gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+
+//declare the task
+gulp.task('sass', function(done) {
+  gulp.src('./stylesheets/chameleon.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 20 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(rename({ extname: '.css' }))
+    .pipe(gulp.dest('./dist/'))
+    .on('end', done);
+});
+
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./stylesheets/**/*.scss', ['sass']);
+});
