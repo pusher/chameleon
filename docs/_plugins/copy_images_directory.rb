b136f0bf0985206ccc1e9959fdaca3e0fd16bf63
Alex Pate <alex.pate@pusher.com>
2016-06-06 15:10:19 +0100

    Added social buttons and helper to use social button images

---
+++ docs/_plugins/copy_images_directory.rb
@@ -0,0 +1,14 @@
+module CopyImagesDirectory
+  class Generator < Jekyll::Generator
+
+    # As the docs get precompiled before being sent up to jekyll,
+    # and they sit inside Chameleon, the images folder needs to be
+    # copied into the docs repo.
+    def generate(site)
+
+       FileUtils.cp_r '../images', 'images'
+
+    end
+    
+  end
+end
