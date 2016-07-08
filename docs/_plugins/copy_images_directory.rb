f522940c88513fe5be40b54e647e16bae502fb09
Alex Pate <alex.pate@pusher.com>
2016-07-08 16:43:06 +0100

    General tweaks

--- docs/_plugins/copy_images_directory.rb
+++ docs/_plugins/copy_images_directory.rb
@@ -1,14 +1,15 @@
-module CopyImagesDirectory
-  class Generator < Jekyll::Generator
+class MyCustomCommand < Jekyll::Command
+  class << self
 
     # As the docs get precompiled before being sent up to jekyll,
     # and they sit inside Chameleon, the images folder needs to be
     # copied into the docs repo.
-    def generate(site)
-
-       # FileUtils.cp_r '../images', 'images'
-
+    def init_with_program(prog)
+      prog.command(:build) do |c|
+      puts "hello world"
+       FileUtils.cp_r '../images', 'images'
+      end
     end
-    
+
   end
 end
