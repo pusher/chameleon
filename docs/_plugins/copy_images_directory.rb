4dffcb3a17bf7517dbed10cd5832d798846ea05f
Alex Pate <alex@alexpate.uk>
2016-09-12 19:45:22 +0100

    Fix docs image dir plugin

--- docs/_plugins/copy_images_directory.rb
+++ docs/_plugins/copy_images_directory.rb
@@ -1,13 +1,20 @@
-class MyCustomCommand < Jekyll::Command
+require 'fileutils'
+
+class MyNewCommand < Jekyll::Command
   class << self
 
     # As the docs get precompiled before being sent up to jekyll,
     # and they sit inside Chameleon, the images folder needs to be
     # copied into the docs repo.
+    if (!File.directory?('images'))
+      FileUtils.cp_r '../images', 'images'
+    end
+
     def init_with_program(prog)
       prog.command(:build) do |c|
-      puts "hello world"
-       FileUtils.cp_r '../images', 'images'
+        c.action do
+          puts "hello world"
+        end
       end
     end
 
